3c6eb9cdf6adea3d2a3d499aa425d4e5da2f15ba
Fel Pabustan <felpabustan93@gmail.com>
2025-04-21 20:38:04 +0800

    feature: update default welcome page

---
+++ resources/js/components/WelcomeHeroImage.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg
+        class="relative w-[448px] max-w-none"
+        viewBox="0 0 440 376"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <!-- Your SVG content here -->
+    </svg>
+</template>
